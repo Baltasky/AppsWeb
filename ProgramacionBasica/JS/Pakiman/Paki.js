815ea576182a7000383310456905de0a60a5974e
Balta Rdz <baltazaar29@gmail.com>
2020-01-23 20:18:35 -0600

    Clases y Arrays en JavaSript

---
+++ ProgramacionBasica/JS/Pakiman/Paki.js
@@ -0,0 +1,15 @@
+var imagenes = [];
+imagenes["Cauchin"] = "imagenes/vaca.png";
+imagenes["Pokacho"] = "imagenes/pollo.png";
+imagenes["Tosinauro"] = "imagenes/cerdo.png"
+
+
+var coleccion = [];
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pokacho", 80, 50));
+coleccion.push(new Pakiman("Tosinauro", 120, 40));
+
+for (const pokis of coleccion) 
+{
+   pokis.mostrar();
+}
